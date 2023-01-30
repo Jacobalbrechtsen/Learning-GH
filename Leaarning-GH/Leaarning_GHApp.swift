7cac81381413bf86f89bfbef28b8ae363b3fa34f
Jacob Albrechtsen <jacob23015@gmail.com>
2023-01-30 15:41:16 +0100

    Initial Commit

---
+++ Leaarning-GH/Leaarning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leaarning_GHApp.swift
+//  Leaarning-GH
+//
+//  Created by Jacob Albrechtsen on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Leaarning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
